f9a61a316730238113367359810c4d79d9fd1cdc
Huu Tung Nguyen <tung.nguyen97@outlook.com>
2024-03-25 18:22:14 +0100

    Add forgot password page

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { Metadata } from 'next'
+import React from 'react';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { Gutter } from '../../_components/Gutter'
-import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
-import { RecoverPasswordForm } from './RecoverPasswordForm'
+import * as RenderParams from '../../_components/RenderParams';
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph';
+import { RecoverPasswordForm } from './RecoverPasswordForm';
 
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
-  )
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams.RenderParams className={classes.params} />
+          <Link href="/login" className={classes.backLink}>
+            <Image src="/assets/icons/arrow-left.svg" alt="back" width={24} height={24} />
+            <p>Back</p>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Forgot Password</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export const metadata: Metadata = {
@@ -22,4 +48,4 @@ export const metadata: Metadata = {
     title: 'Recover Password',
     url: '/recover-password',
   }),
-}
+};
