801d362a245d7bb3fa64fe6b2f50b10282e73667
Huu Tung Nguyen <tung.nguyen97@outlook.com>
2024-03-25 13:42:57 +0100

    Add create account page

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -1,28 +1,53 @@
-import React from 'react'
-import { Metadata } from 'next'
+import React from 'react';
+import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { Gutter } from '../../_components/Gutter'
-import { RenderParams } from '../../_components/RenderParams'
-import { getMeUser } from '../../_utilities/getMeUser'
-import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
-import CreateAccountForm from './CreateAccountForm'
+import { Gutter } from '../../_components/Gutter';
+import { RenderParams } from '../../_components/RenderParams';
+import { getMeUser } from '../../_utilities/getMeUser';
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph';
+import CreateAccountForm from './CreateAccountForm';
 
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
 export default async function CreateAccount() {
   await getMeUser({
     validUserRedirect: `/account?warning=${encodeURIComponent(
       'Cannot create a new account while logged in, please log out and try again.',
     )}`,
-  })
+  });
 
   return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
-  )
+    <section className={classes.createAccount}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Create Account</h3>
+            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
+          </div>
+
+          <p>Please enter details</p>
+
+          <CreateAccountForm />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export const metadata: Metadata = {
@@ -32,4 +57,4 @@ export const metadata: Metadata = {
     title: 'Account',
     url: '/account',
   }),
-}
+};
