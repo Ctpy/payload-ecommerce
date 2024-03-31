17e021a999c3445d44b0ac18d0917dd473d129be
Huu Tung Nguyen <tung.nguyen97@outlook.com>
2024-03-31 16:43:50 +0200

    Adjust docker-compose

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -31,7 +31,6 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
           onClick={() => (window.location.href = '/login')}
         />
       )}
-      {user && <CartLink />}
     </nav>
   );
 };
